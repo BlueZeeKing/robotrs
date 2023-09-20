1ab4cb4f813fee7e90597fa9c74b8450b5c155b5
BlueZeeKing <github@blueish.dev>
2023-09-20 13:15:07 -0400

    Use global WPI_VERSION

--- build-utils/src/lib.rs
+++ build-utils/src/lib.rs
@@ -6,6 +6,7 @@ pub mod artifact;
 pub mod zip;
 
 static CLIENT: OnceLock<Client> = OnceLock::new();
+pub const WPI_VERSION: &str = "2023.4.3";
 
 pub fn get_client() -> &'static Client {
     CLIENT.get_or_init(|| Client::new())
