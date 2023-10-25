725487baa7809f280058a2307dff4e008cfe7fd7
BlueZeeKing <github@blueish.dev>
2023-10-25 12:26:37 -0400

    Add ratio constructor

--- units/src/ratio.rs
+++ units/src/ratio.rs
@@ -8,17 +8,26 @@ pub struct Fraction(f32);
 
 pub trait Ratio: Clone + Copy {
     fn get_ratio(self) -> f32;
+    fn new(val: f32) -> Self;
 }
 
 impl Ratio for Percent {
     fn get_ratio(self) -> f32 {
         self.0 / 100.0
     }
+
+    fn new(val: f32) -> Self {
+        Self(val)
+    }
 }
 
 impl Ratio for Fraction {
     fn get_ratio(self) -> f32 {
         self.0
+    }
+
+    fn new(val: f32) -> Self {
+        Self(val)
     }
 }
 
