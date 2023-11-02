ab7e3af0084ab46faf062d52891f0b52cde828d9
BlueZeeKing <github@blueish.dev>
2023-11-02 14:08:26 -0400

    Remove unneeded imports

--- robotrs/src/robot.rs
+++ robotrs/src/robot.rs
@@ -1,9 +1,3 @@
-use std::pin::Pin;
-
-use futures::Future;
-
-pub type Fut = Pin<Box<dyn Future<Output = anyhow::Result<()>> + 'static>>;
-
 pub trait AsyncRobot {
     async fn get_auto_future(&self) -> anyhow::Result<()>;
 
