99bf09b9a6a43fe6d9194a76ac022b8ea7cf71d2
BlueZeeKing <github@blueish.dev>
2024-01-01 23:04:31 -0500

    Fix time

--- nt/src/main.rs
+++ nt/src/main.rs
@@ -1,26 +1,27 @@
-use nt_rs::{time::init_time, tokio::TokioBackend, NetworkTableClient, Publisher};
-
-#[tokio::main]
-async fn main() {
-    init_time();
-
-    let (client, task) = NetworkTableClient::new::<TokioBackend>("localhost", "rust")
-        .await
-        .unwrap();
-
-    {
-        let client = client;
-
-        let main = tokio::spawn(client.main_task::<TokioBackend>());
-
-        let publisher: Publisher<String> = client
-            .publish("/FMSInfo/GameSpecificMessage".to_owned())
-            .unwrap();
-
-        publisher.set("Hello, World!".to_owned()).unwrap();
-
-        main.abort(); // End the main recieve loop
-    }
-
-    task.await.unwrap(); // Wait for the backend to stop
-}
+// use nt_rs::{time::init_time, tokio::TokioBackend, NetworkTableClient, Publisher};
+//
+// #[tokio::main]
+// async fn main() {
+//     init_time();
+//
+//     let (client, task) = NetworkTableClient::new::<TokioBackend>("localhost", "rust")
+//         .await
+//         .unwrap();
+//
+//     {
+//         let client = client;
+//
+//         let main = tokio::spawn(client.main_task::<TokioBackend>());
+//
+//         let publisher: Publisher<String> = client
+//             .publish("/FMSInfo/GameSpecificMessage".to_owned())
+//             .unwrap();
+//
+//         publisher.set("Hello, World!".to_owned()).unwrap();
+//
+//         main.abort(); // End the main recieve loop
+//     }
+//
+//     task.await.unwrap(); // Wait for the backend to stop
+// }
+fn main() {}
--- nt/src/time.rs
+++ nt/src/time.rs
@@ -1,12 +1,23 @@
 use std::{sync::OnceLock, time::Instant};
 
+#[cfg(not(feature = "wasm"))]
 static START_TIME: OnceLock<Instant> = OnceLock::new();
 
 pub fn get_time() -> u64 {
     // TODO: Use FPGA when on robot
-    START_TIME.get_or_init(Instant::now).elapsed().as_micros() as u64
+    #[cfg(not(feature = "wasm"))]
+    {
+        START_TIME.get_or_init(Instant::now).elapsed().as_micros() as u64
+    }
+    #[cfg(feature = "wasm")]
+    {
+        let millis = web_sys::window().unwrap().performance().unwrap().now();
+
+        (millis * 1000.0) as u64
+    }
 }
 
 pub fn init_time() {
+    #[cfg(not(feature = "wasm"))]
     START_TIME.get_or_init(Instant::now);
 }
