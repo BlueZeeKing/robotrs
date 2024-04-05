f725b1b4b128e4248c127c34e442a78a7c824552
BlueZeeKing <github@blueish.dev>
2024-04-04 21:11:29 -0400

    Change the way odometry handles rotation

--- math/src/odometry.rs
+++ math/src/odometry.rs
@@ -15,20 +15,24 @@ pub struct Odometry<K: Kinematics> {
 pub(crate) struct InnerOdometry<K: Kinematics> {
     kinematics: K,
     pose: Cell<Vector3<f32>>,
+    last_rotation: Cell<f32>,
 }
 
 impl<K: Kinematics> InnerOdometry<K> {
-    pub(crate) fn new(kinematics: K, starting_pose: Vector3<f32>) -> Self {
+    pub(crate) fn new(kinematics: K, starting_pose: Vector3<f32>, current_heading: f32) -> Self {
         Self {
             kinematics,
             pose: Cell::new(starting_pose),
+            last_rotation: Cell::new(current_heading),
         }
     }
 
     pub(crate) fn update(&self, value: K::State, current_heading: f32) {
-        let displacement = self.kinematics.forward(value);
+        let displacement = self.kinematics.forward(value).fixed_resize::<2, 1>(0.0);
 
-        let delta_theta = displacement.z;
+        let delta_theta = current_heading - self.last_rotation.get();
+
+        self.last_rotation.set(current_heading);
 
         let sin_theta = delta_theta.sin();
         let cos_theta = delta_theta.cos();
@@ -36,26 +40,23 @@ impl<K: Kinematics> InnerOdometry<K> {
         let pose_exponential_matrix = if delta_theta.abs() > f32::EPSILON {
             let sin_theta_expr = sin_theta / delta_theta;
 
-            matrix![sin_theta_expr, (cos_theta - 1.0) / delta_theta, 0.0;
-                (1.0 - cos_theta) / delta_theta, sin_theta_expr, 0.0;
-                0.0, 0.0, 1.0]
+            matrix![sin_theta_expr, (cos_theta - 1.0) / delta_theta;
+            (1.0 - cos_theta) / delta_theta, sin_theta_expr
+            ]
         } else {
             let sin_theta_expr = 1.0 - delta_theta.powi(2) / 6.0;
 
-            matrix![sin_theta_expr, -delta_theta / 2.0, 0.0;
-                delta_theta / 2.0, sin_theta_expr, 0.0;
-                0.0, 0.0, 1.0]
+            matrix![sin_theta_expr, -delta_theta / 2.0;
+            delta_theta / 2.0, sin_theta_expr
+            ]
         };
 
-        let mut rotation_matrix = Rotation2::new(current_heading)
-            .matrix()
-            .fixed_resize::<3, 3>(0.0);
+        let rotation = Rotation2::new(self.pose.get().z + delta_theta);
 
-        *rotation_matrix.index_mut((2, 2)) = 1.0;
+        let final_displacement = rotation.matrix() * pose_exponential_matrix * displacement;
 
-        let final_displacement = rotation_matrix * pose_exponential_matrix * displacement;
-
-        self.pose.set(self.pose.get() + final_displacement);
+        self.pose
+            .set(self.pose.get() + final_displacement.fixed_resize::<3, 1>(delta_theta));
     }
 
     pub(crate) fn get_pose(&self) -> Vector3<f32> {
@@ -68,9 +69,13 @@ impl<K: Kinematics> InnerOdometry<K> {
 }
 
 impl<K: Kinematics> Odometry<K> {
-    pub fn new(kinematics: K, starting_pose: Vector3<f32>) -> Self {
+    pub fn new(kinematics: K, starting_pose: Vector3<f32>, current_heading: f32) -> Self {
         Self {
-            inner: Rc::new(InnerOdometry::new(kinematics, starting_pose)),
+            inner: Rc::new(InnerOdometry::new(
+                kinematics,
+                starting_pose,
+                current_heading,
+            )),
         }
     }
 
