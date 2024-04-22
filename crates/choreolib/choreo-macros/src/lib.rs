769943598f7954dd0cc0f47a59b8284738bc8989
BlueZeeKing <github@blueish.dev>
2024-04-22 18:59:57 -0400

    Add choreo

---
+++ crates/choreolib/choreo-macros/src/lib.rs
@@ -0,0 +1,168 @@
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::Read,
+    path::PathBuf,
+    time::Duration,
+};
+
+use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use serde::Deserialize;
+use serde_json::from_str;
+use syn::{parse, LitStr};
+
+#[derive(Debug, Deserialize)]
+struct Path {
+    samples: Vec<TrajectoryPoint>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct TrajectoryPoint {
+    x: f32,
+    y: f32,
+    heading: f32,
+    angular_velocity: f32,
+    velocity_x: f32,
+    velocity_y: f32,
+    timestamp: f32,
+}
+
+#[derive(Debug)]
+struct PartialPathEntry {
+    root: Option<Path>,
+    values: Vec<(u32, Path)>,
+}
+
+/// This macros searches a directory (`deploy/choreo` by default) for `.traj` files and serializes
+/// them into `Path`s. The macros creates a public `paths` module. Each submodule within that
+/// corresponds to one path. There is a `const` called `PATH` within each submodule correspoinding
+/// to the entire path. There are is also `PATH1`, `PATH2`, and so one, which correspond with the
+/// different segments of the path. This does mean that trajectory names must be valid Rust
+/// identifiers.
+///
+/// # Example
+///
+/// ```rust
+/// choreo!(); // Load from default directory
+/// ```
+///
+/// ```rust
+/// choreo!("/some/arbitrary/directory"); // Load from arbitrary directory
+/// ```
+#[proc_macro]
+pub fn choreo(item: TokenStream) -> TokenStream {
+    let path = parse::<LitStr>(item)
+        .ok()
+        .map(|val| PathBuf::from(val.value()))
+        .unwrap_or_else(|| {
+            PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("deploy/choreo")
+        });
+
+    let paths = fs::read_dir(path).expect("Could not read trajectory directory");
+
+    let mut trajectories = HashMap::new();
+
+    for file in paths {
+        let file = file.expect("Failed to read directory entry");
+
+        let name = file.file_name();
+        let name = name.to_str().unwrap().trim_end_matches(".traj");
+        let mut parts = name.split('.');
+
+        let name = parts.next().unwrap();
+        let num = parts.next().map(|val| val.parse::<u32>().unwrap());
+
+        if !trajectories.contains_key(name) {
+            trajectories.insert(
+                name.to_owned(),
+                PartialPathEntry {
+                    root: None,
+                    values: Vec::new(),
+                },
+            );
+        }
+
+        let mut file = File::open(file.path()).expect("Could not open trajectory file");
+
+        let mut val = String::new();
+        file.read_to_string(&mut val)
+            .expect("Could not read trajectory file");
+
+        let path: Path = from_str(&val).expect("Could not parse trajectory file");
+
+        if let Some(num) = num {
+            trajectories.get_mut(name).unwrap().values.push((num, path));
+        } else {
+            trajectories.get_mut(name).unwrap().root = Some(path);
+        }
+    }
+
+    let paths = trajectories.into_iter().map(|(name, entry)| {
+        let name = Ident::new(&name, Span::call_site());
+
+        let root_samples = entry
+            .root
+            .unwrap()
+            .samples
+            .into_iter()
+            .map(sample_to_tokens);
+
+        let other_paths = entry.values.into_iter().map(|(num, path)| {
+            let name = Ident::new(&format!("PATH{}", num), Span::call_site());
+
+            let samples = path.samples.into_iter().map(sample_to_tokens);
+
+            quote! {
+                pub const #name: ::choreolib::Path<'static> = ::choreolib::Path {
+                    samples: &[#(#samples),*],
+                };
+            }
+        });
+
+        quote! {
+            pub mod #name {
+                pub const PATH: ::choreolib::Path<'static> = ::choreolib::Path {
+                    samples: &[#(#root_samples),*],
+                };
+
+                #(#other_paths)*
+            }
+        }
+    });
+
+    quote! {
+        #[allow(warnings)]
+        pub mod paths {
+            #(#paths)*
+        }
+    }
+    .into()
+}
+
+fn sample_to_tokens(sample: TrajectoryPoint) -> proc_macro2::TokenStream {
+    let x = sample.x;
+    let y = sample.y;
+    let heading = sample.heading;
+    let angular_velocity = sample.angular_velocity;
+    let velocity_x = sample.velocity_x;
+    let velocity_y = sample.velocity_y;
+    let time = Duration::from_secs_f32(sample.timestamp);
+    let secs = time.as_secs();
+    let nanos = time.subsec_nanos();
+
+    quote! {
+        ::choreolib::TrajectoryPoint {
+            x: #x,
+            y: #y,
+            heading: #heading,
+            angular_velocity: #angular_velocity,
+            velocity_x: #velocity_x,
+            velocity_y: #velocity_y,
+            timestamp: ::std::time::Duration::new(#secs, #nanos),
+
+        }
+    }
+}
---
+++ crates/choreolib/src/lib.rs
@@ -0,0 +1,202 @@
+use std::time::Duration;
+
+use nalgebra::Vector3;
+use robotrs::{
+    math::{Controller, State},
+    time::get_time,
+    yield_now,
+};
+
+pub use choreo_macros::choreo;
+
+/// This represents an entire path
+#[derive(Debug)]
+pub struct Path<'a> {
+    pub samples: &'a [TrajectoryPoint],
+}
+
+/// One singular point along a [Path]
+#[derive(Debug)]
+pub struct TrajectoryPoint {
+    pub x: f32,
+    pub y: f32,
+    pub heading: f32,
+    pub angular_velocity: f32,
+    pub velocity_x: f32,
+    pub velocity_y: f32,
+    pub timestamp: Duration,
+}
+
+fn linear_interpolate(
+    first: f32,
+    first_time: f32,
+    second_time: f32,
+    second: f32,
+    time: f32,
+) -> f32 {
+    (first * (second_time - time) + second * (first_time - time)) / (second_time - first_time)
+}
+
+fn interpolate_point(a: &TrajectoryPoint, b: &TrajectoryPoint, time: Duration) -> TrajectoryPoint {
+    let time_secs = time.as_secs_f32();
+    let first_time = a.timestamp.as_secs_f32();
+    let second_time = b.timestamp.as_secs_f32();
+
+    TrajectoryPoint {
+        x: linear_interpolate(a.x, first_time, second_time, b.x, time_secs),
+        y: linear_interpolate(a.y, first_time, second_time, b.y, time_secs),
+        heading: linear_interpolate(a.heading, first_time, second_time, b.heading, time_secs),
+        angular_velocity: linear_interpolate(
+            a.angular_velocity,
+            first_time,
+            second_time,
+            b.angular_velocity,
+            time_secs,
+        ),
+        velocity_x: linear_interpolate(
+            a.velocity_x,
+            first_time,
+            second_time,
+            b.velocity_x,
+            time_secs,
+        ),
+        velocity_y: linear_interpolate(
+            a.velocity_y,
+            first_time,
+            second_time,
+            b.velocity_y,
+            time_secs,
+        ),
+        timestamp: time,
+    }
+}
+
+/// Follow the given path and pass the points into a closure. Consumer gets called every loop,
+/// which defaults to every 20ms. The points are generated using linear interpolation from the
+/// path. See [simple_controller] for an easy way to consume the points.
+///
+/// # Example
+///
+/// ```rust
+/// follow_path(
+///     &paths::Test::PATH,
+///     simple_controller(
+///         || odometry.get_pose(),
+///         LinearController::default(),
+///         LinearController::default(),
+///         AngularController::default(),
+///         |x, y, heading| drivetrain.set_input_raw(Vector2::new(x, y), heading),
+///     ),
+/// )
+/// .await
+///```
+pub async fn follow_path<O, E>(path: &Path<'_>, mut consumer: O) -> Result<(), E>
+where
+    O: FnMut(&TrajectoryPoint) -> Result<(), E>,
+{
+    let mut current_idx = 0;
+
+    loop {
+        let time = get_time();
+
+        if time == path.samples[current_idx].timestamp {
+            consumer(&path.samples[current_idx])?;
+            yield_now().await;
+            continue;
+        }
+
+        if current_idx == path.samples.len() - 1 {
+            break Ok(());
+        }
+
+        if time >= path.samples[current_idx].timestamp {
+            current_idx += 1;
+            continue;
+        }
+
+        consumer(&interpolate_point(
+            &path.samples[current_idx],
+            &path.samples[current_idx + 1],
+            time,
+        ))?;
+        yield_now().await;
+    }
+}
+
+fn deriver(mut initial: Option<f32>) -> impl FnMut(f32) -> f32 {
+    let mut last_time = get_time();
+
+    move |value| {
+        let curr_time = get_time();
+        let Some(last_val) = initial else {
+            last_time = curr_time;
+            initial = Some(value);
+            return 0.0;
+        };
+
+        let res = (value - last_val) / (curr_time.as_secs_f32() - last_time.as_secs_f32());
+
+        last_time = curr_time;
+        initial = Some(value);
+
+        return res;
+    }
+}
+
+/// This function takes in a closure that returns the robot's current pose, 3 controllers for each
+/// axis, and a closure that consumes the final 3 control outputs. See [follow_path] for example.
+pub fn simple_controller<E, O1, O2, O3>(
+    mut pose: impl FnMut() -> Vector3<f32>,
+    mut x_controller: impl Controller<State, O1>,
+    mut y_controller: impl Controller<State, O2>,
+    mut angle_controller: impl Controller<State, O3>,
+    mut consumer: impl FnMut(O1, O2, O3) -> Result<(), E>,
+) -> impl FnMut(&TrajectoryPoint) -> Result<(), E> {
+    let mut x_velocity = deriver(None);
+    let mut y_velocity = deriver(None);
+    let mut angle_velocity = deriver(None);
+
+    move |point| {
+        let current_pose = pose();
+        let x = current_pose.x;
+        let y = current_pose.y;
+        let heading = current_pose.z;
+
+        let x_vel = x_velocity(x);
+        let y_vel = y_velocity(x);
+        let angle_vel = angle_velocity(x);
+
+        consumer(
+            x_controller.calculate(
+                &State {
+                    position: x,
+                    velocity: x_vel,
+                },
+                &State {
+                    position: point.x,
+                    velocity: point.velocity_x,
+                },
+            ),
+            y_controller.calculate(
+                &State {
+                    position: y,
+                    velocity: y_vel,
+                },
+                &State {
+                    position: point.y,
+                    velocity: point.velocity_y,
+                },
+            ),
+            angle_controller.calculate(
+                &State {
+                    position: heading,
+                    velocity: angle_vel,
+                },
+                &State {
+                    position: point.heading,
+                    velocity: point.angular_velocity,
+                },
+            ),
+        )
+    }
+}
