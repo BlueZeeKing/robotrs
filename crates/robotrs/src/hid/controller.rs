156eecb8af02c4aa107c1a5a0c770e91fc130968
BlueZeeKing <github@blueish.dev>
2024-04-29 10:57:13 -0400

    Add PS4

--- crates/robotrs/src/hid/controller.rs
+++ crates/robotrs/src/hid/controller.rs
@@ -108,3 +108,56 @@ define_povs!(
     left = 0 => 270,
     up_left = 0 => 315
 );
+
+#[derive(Clone, Copy)]
+pub struct PS4Controller {
+    joystick: Joystick,
+}
+
+impl PS4Controller {
+    pub fn new(num: u32) -> Result<Self> {
+        Ok(Self {
+            joystick: Joystick::new(num)?,
+        })
+    }
+}
+
+define_buttons!(
+    PS4Controller,
+    square = 0,
+    cross = 1,
+    circle = 2,
+    triangle = 3,
+    l1 = 4,
+    r1 = 5,
+    l2_button = 6,
+    r2_button = 7,
+    share = 8,
+    options = 9,
+    l3 = 10,
+    r3 = 11,
+    ps = 12,
+    touchpad = 13
+);
+
+define_axes!(
+    PS4Controller,
+    wait_left_x / left_x = 0,
+    wait_left_y / left_y = 1,
+    wait_right_x / right_x = 2,
+    wait_right_y / right_y = 5,
+    wait_l2_axis / l2_axis = 3,
+    wait_r2_axis / r2_axis = 4
+);
+
+define_povs!(
+    PS4Controller,
+    up = 0 => 0,
+    up_right = 0 => 45,
+    right = 0 => 90,
+    down_right = 0 => 135,
+    down = 0 => 180,
+    down_left = 0 => 225,
+    left = 0 => 270,
+    up_left = 0 => 315
+);
