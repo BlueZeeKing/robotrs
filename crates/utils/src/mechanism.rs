33f68891eeb30ac34ec3abf3bc9acfd41b62cda9
BlueZeeKing <github@blueish.dev>
2024-08-01 15:37:13 +0200

    Rewrite mechanism to not use `futures`
    Also add logs

--- crates/utils/src/mechanism.rs
+++ crates/utils/src/mechanism.rs
@@ -1,10 +1,11 @@
 use flume::{Receiver, Sender};
-use futures::{channel::oneshot, select, FutureExt};
+use futures::channel::oneshot;
+use futures_concurrency::future::Race;
 use robotrs::{
     control::ControlSafe, math::Controller, motor::MotorController, scheduler, yield_now,
 };
 use std::fmt::Debug;
-use tracing::{debug, error, warn};
+use tracing::{debug, error, instrument, span, trace, warn, Instrument, Level};
 
 struct MechanismRequest<I> {
     state: I,
@@ -21,7 +22,6 @@ pub struct Mechanism<I: 'static, E: 'static + Debug> {
     sender: Sender<MechanismRequest<I>>,
     errors: Receiver<E>,
     stop: Sender<()>,
-    name: &'static str,
 }
 
 impl<I: 'static, E: Debug + 'static> Mechanism<I, E> {
@@ -36,7 +36,6 @@ impl<I: 'static, E: Debug + 'static> Mechanism<I, E> {
         mut supplier: Supply,
         mut consumer: Consume,
         mut at_setpoint: Check,
-        name: &'static str,
     ) -> Self {
         let (sender, receiver): (Sender<MechanismRequest<I>>, Receiver<MechanismRequest<I>>) =
             flume::unbounded();
@@ -45,73 +44,91 @@ impl<I: 'static, E: Debug + 'static> Mechanism<I, E> {
 
         let (stop_sender, stop_receiver) = flume::bounded(1);
 
-        scheduler::spawn(async move {
-            loop {
-                select! { _ = async {
-                        let Ok(request) = receiver.recv_async().await else {
-                            debug!("Stopping {} because sender is gone", name);
-                            return;
-                        };
+        scheduler::spawn(
+            async move {
+                loop {
+                    let should_break = (
+                        async {
+                            trace!("Waiting for control request");
+                            let Ok(request) = receiver.recv_async().await else {
+                                debug!("stopping, sender is gone");
+                                return true;
+                            };
+                            trace!("Got control request");
 
-                        let mut response = Some(request.response);
+                            let mut response = Some(request.response);
 
-                        loop {
-                            let result: Result<(), E> = try {
-                                let current_state = supplier()?;
+                            loop {
+                                let result: Result<(), E> = try {
+                                    let current_state = supplier()?;
 
-                                consumer(MechanismState::Value(controller.calculate(
-                                    &current_state,
-                                    &request.state,
-                                )))?;
+                                    consumer(MechanismState::Value(
+                                        controller.calculate(&current_state, &request.state),
+                                    ))?;
 
-                                if at_setpoint(&current_state, &request.state) {
-                                    if let Some(response) = response.take() {
-                                        if response.send(()).is_err() {
-                                            warn!("Mechanism response channel has been closed");
+                                    if at_setpoint(&current_state, &request.state) {
+                                        if let Some(response) = response.take() {
+                                            trace!("At setpoint, alerting origin");
+                                            if response.send(()).is_err() {
+                                                warn!("Mechanism response channel has been closed");
+                                            }
                                         }
                                     }
-                                }
-                            };
+                                };
 
-                            if let Err(err) = result {
-                                error!("A mechanism has encountered an error: {:?}", err);
-                                if matches!(errors_sender.try_send(err), Err(flume::TrySendError::Disconnected(_))) {
-                                    debug!("Stopping {} because error sender is gone", name);
-                                    break;
+                                if let Err(err) = result {
+                                    error!("Encountered an error: {:?}", err);
+                                    if matches!(
+                                        errors_sender.try_send(err),
+                                        Err(flume::TrySendError::Disconnected(_))
+                                    ) {
+                                        debug!("Stopping, error receiver is gone");
+                                        return true;
+                                    }
                                 }
-                            }
 
-                            yield_now().await;
-                        }
-                    }.fuse() => {
-                        debug!("Stopping {} because main loop stopped (error should be above)", name);
-                        break;
-                    },
-                    _ = stop_receiver.recv_async() => {
-                        if let Err(err) = consumer(MechanismState::Stop) {
-                            error!(
-                                "A mechanism has encountered an error while stopping: {:?}",
-                                err
-                            );
-                            if matches!(errors_sender.try_send(err), Err(flume::TrySendError::Disconnected(_))) {
-                                debug!("Stopping {} because error sender is gone", name);
-                                break;
+                                yield_now().await;
                             }
+                        },
+                        async {
+                            let _ = stop_receiver.recv_async().await; // will be caught later anyway
+                            trace!("Stop request received");
+
+                            false
+                        },
+                    )
+                        .race()
+                        .await;
+
+                    if let Err(err) = consumer(MechanismState::Stop) {
+                        error!("Encountered an error while stopping: {:?}", err);
+                        if matches!(
+                            errors_sender.try_send(err),
+                            Err(flume::TrySendError::Disconnected(_))
+                        ) {
+                            debug!("Stopping, error receiver is gone");
+                            break;
                         }
                     }
-                };
+
+                    if should_break {
+                        trace!("Stopping mechanism");
+                        break;
+                    }
+                }
             }
-        })
+            .instrument(span!(Level::TRACE, "mechanism task")),
+        )
         .detach();
 
         Self {
             sender,
             errors: errors_receiver,
             stop: stop_sender,
-            name,
         }
     }
 
+    #[instrument(skip_all, name = "mechanism_set")]
     pub async fn set(&mut self, state: I) {
         self.stop();
 
@@ -122,23 +139,28 @@ impl<I: 'static, E: Debug + 'static> Mechanism<I, E> {
                 state,
                 response: response_sender,
             })
-            .unwrap_or_else(|_| panic!("Could not send request in {}", self.name));
+            .unwrap_or_else(|_| {
+                error!("Could not send request mechanism");
+                panic!("Could not send request mechanism");
+            });
 
-        response_receiver
-            .await
-            .unwrap_or_else(|_| panic!("Failed waiting to hit setpoint in {}", self.name));
+        response_receiver.await.unwrap_or_else(|_| {
+            error!("Failed waiting to hit setpoint mechanism");
+            panic!("Failed waiting to hit setpoint mechanism");
+        });
     }
 
-    pub async fn errors(&self) -> &Receiver<E> {
+    pub fn errors(&self) -> &Receiver<E> {
         &self.errors
     }
 }
 
 impl<I: 'static, E: Debug + 'static> ControlSafe for Mechanism<I, E> {
     fn stop(&mut self) {
-        self.stop
-            .try_send(())
-            .unwrap_or_else(|_| panic!("Failed stopping {}", self.name));
+        self.stop.try_send(()).unwrap_or_else(|_| {
+            error!("Failed stopping mechanism");
+            panic!("Failed stopping mechanism");
+        });
     }
 }
 
